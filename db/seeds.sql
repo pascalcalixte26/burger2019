0e71eee45dedfa29c8f78e97c3b2f47c4dab796b
Pascal <pascal.calixte26@gmail.com>
2019-06-19 19:50:04 -0400

    another commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('California burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Veggie burger');
---
+++ public/assets/js/burger.js
@@ -0,0 +1,50 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    console.log("this is the id", id);
+    var newDevour = $(this).data("devou");
+    console.log("it worked");
+
+    var newDevourState = {
+      devoured: 1
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("change devour to", newDevour);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    console.log("added burger");
+
+    var newBurger = {
+      name: $("#ca").val().trim(),  
+      devoured: 0    
+    };
+
+    console.log(newBurger);
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+})
